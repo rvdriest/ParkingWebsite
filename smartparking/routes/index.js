fd3cf3d08587a6d7ec53b3203b0acdb5924c500d
Richardd01 <richard.vandriest@outlook.com>
2020-01-27 11:56:52 +0100

    User interface responsive

--- smartparking/routes/index.js
+++ smartparking/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { verdieping0: '5', verdieping1: '16' });
 });
 
 module.exports = router;
