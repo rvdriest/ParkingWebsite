f7c3fc88484b6a8fbb5208623db99563e8f8cd42
rvdriest <richard.vandriest@outlook.com>
2020-01-29 00:04:51 +0100

    Connectie met backend

--- smartparking/app.js
+++ smartparking/app.js
@@ -3,6 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var schedule = require('node-schedule');
+const {
+  Pool,
+  Client
+} = require('pg');
+
+const connectionString = 'postgres://vfqpuona:rrSckG8ApXOhGWuZfzQ2M3P_3sOISmxe@dumbo.db.elephantsql.com:5432/vfqpuona';
+
+const client = new Client({
+  connectionString: connectionString
+});
+
+client.connect();
+
+var aantalPlekkenVerdieping0 = 0;
+var aantalPlekkenVerdieping1 = 0;
+var aantalPlekkenVerdieping2 = 0;
+var aantalPlekkenVerdieping3 = 0;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -15,20 +33,61 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+client.connect();
+
+var j = schedule.scheduleJob('*/10 * * * * *', function (fireDate) {
+  client.query('SELECT * FROM "public"."Parkingspots"', (err, result) => {
+    if (err)
+      throw err;
+    console.log(result.rows);
+    aantalPlekkenVerdieping0 = 0;
+    aantalPlekkenVerdieping1 = 0;
+    aantalPlekkenVerdieping2 = 0;
+    aantalPlekkenVerdieping3 = 0;
+    result.rows.forEach(function(element) {
+      if(element['Bezet']) {
+        if(element['Verdieping'] == 0) {
+          aantalPlekkenVerdieping0++
+        }
+        else if(element['Verdieping'] == 1) {
+          aantalPlekkenVerdieping1++;
+        }
+        else if(element['Verdieping'] == 2) {
+          aantalPlekkenVerdieping2++;
+        }
+        else if(element['Verdieping'] == 3) {
+          aantalPlekkenVerdieping3++;
+        }
+      }
+    });
+  });
+});
+
+app.use('/', function (req, res) {
+
+  res.render('index', {
+    verdieping0: aantalPlekkenVerdieping0,
+    verdieping1: aantalPlekkenVerdieping1
+  });
+
+});
+
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- smartparking/routes/index.js
+++ smartparking/routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+const { Pool, Client } = require('pg');
+
+const connectionString = 'postgres://vfqpuona:rrSckG8ApXOhGWuZfzQ2M3P_3sOISmxe@dumbo.db.elephantsql.com:5432/vfqpuona';
+
+const client = new Client({
+  connectionString: connectionString
+});
+
+
+var aantalPlekkenVerdieping0 = 0;
+var aantalPlekkenVerdieping1 = 0;
+var aantalPlekkenVerdieping2 = 0;
+var aantalPlekkenVerdieping3 = 0;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { verdieping0: '5', verdieping1: '16' });
+router.get('/', (req, res, next) => {
+  client.connect();
+  client.query('SELECT * FROM "public"."Parkingspots"', (err, result) => {
+    if(err)
+      throw err;
+    console.log(result.rows);
+    res.render('index', { verdieping0: aantalPlekkenVerdieping0, verdieping1: aantalPlekkenVerdieping1 });
+    client.end();
+  });
 });
 
 module.exports = router;
+
+/*result.rows.forEach(function(element) {
+      if(element['Bezet']) {
+        if(element['Verdieping'] == 0) {
+          aantalPlekkenVerdieping0++
+        }
+        else if(element['Verdieping'] == 1) {
+          aantalPlekkenVerdieping1++;
+        }
+        else if(element['Verdieping'] == 2) {
+          aantalPlekkenVerdieping2++;
+        }
+        else if(element['Verdieping'] == 3) {
+          aantalPlekkenVerdieping3++;
+        }
+      }
+    });*/
